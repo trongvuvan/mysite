3ec38dc1555b11f92d093634188b70cf2d9c588a
trongvuvan <trong.vuvan0810@gmail.com>
2022-08-08 18:35:33 +0700

    Django Tutorial Part 6: Generic list and detail views

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView
+
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
@@ -9,7 +13,6 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
     num_authors = Author.objects.count()
-    
     num_genre = Genre.objects.count()
     
     context = {
@@ -21,3 +24,27 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+    
+class BookDetailView(generic.DetailView):
+    model = Book
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['copy_list'] = BookInstance.objects.all().filter(book=self.object)
+        return context
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['author_book_list'] = Book.objects.all().filter(author=self.object)
+        return context
+    
